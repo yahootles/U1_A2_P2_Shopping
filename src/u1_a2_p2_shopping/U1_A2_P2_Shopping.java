42b098bd2c460168eaf96a3c90bb90ce49d0d7c0
antho6229 <antho6229@7KSM6Y1.Student.UGDSB.ED>
2018-10-03 09:32:26 -0400

    Created program that outputs price based on number the user enters.

---
+++ src/u1_a2_p2_shopping/U1_A2_P2_Shopping.java
@@ -0,0 +1,53 @@
+/*
+ * Andrew Thompson
+ * October 3, 2018
+ * This program calculates and displays the price of a number of items
+ */
+
+package u1_a2_p2_shopping;
+
+/**
+ *
+ * @author antho6229
+ */
+
+import java.util.Scanner;
+import java.text.DecimalFormat;
+
+public class U1_A2_P2_Shopping {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //declare constant
+        final double TAX_RATE = 0.13;
+        
+        //declare variables
+        int numUSB, numKeyboard, numMice;
+        double purPrice, taxAmount, totalPrice;
+        double usbPrice = 19.99;
+        double keyboardPrice = 49.99;
+        double mousePrice = 25.99;
+        Scanner keyedInput = new Scanner(System.in);
+        DecimalFormat df = new DecimalFormat("$###.##");
+        
+        //ask for number of each
+        System.out.println("Enter the number of USBs you wish to purchase:");
+        numUSB = keyedInput.nextInt();
+        
+        System.out.println("Enter the number of keyboards you wish to purchase:");
+        numKeyboard = keyedInput.nextInt();
+        
+        System.out.println("Enter the number of mice you wish to purchase:");
+        numMice = keyedInput.nextInt();
+        
+        //calculate
+        purPrice = (numUSB * usbPrice) + (numKeyboard * keyboardPrice) + (numMice * mousePrice);
+        taxAmount = purPrice * TAX_RATE;
+        totalPrice = purPrice + taxAmount;
+        
+        System.out.println("\n\nBill\n*************\n\nPrice:\t\t" + df.format(purPrice) + "\nHST:\t\t" + df.format(taxAmount) + "\nTotal price:\t" + df.format(totalPrice));
+    }
+    
+}
